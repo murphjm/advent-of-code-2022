3e88fb3386d3fa704514ab2904de8b75093e7dc5
Justin Murphy <jmsconsulting@gmail.com>
2022-12-01 18:47:48 -0800

    First commit

---
+++ 1/src/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ifstream file ("/Users/jmurphy/git/aoc2022/assets/problem1_input.txt"); 
+    vector<int> elves;
+    string line;
+    int calories = 0;
+
+    if(file.is_open()) 
+    {
+        while (getline(file, line)) {
+            if(strcmp(line.c_str(), "") == 0) {
+                elves.push_back(calories);
+                calories = 0;
+            } else {
+                calories += atoi(line.c_str());
+            }
+        }
+    }
+    else
+        cout<<"Couldn't open the file\n";
+
+    sort(elves.begin(), elves.end(), greater<int>());
+    printf("Highest calories: %d\n", elves[0]);
+    printf("Highest calories of top 3 elves: %d\n", elves[0] + elves[1] + elves[2]);
+}
