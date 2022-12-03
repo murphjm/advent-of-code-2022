3e5c86e0bb60c2984828026f008694ca340b6bfa
Justin Murphy <jmsconsulting@gmail.com>
2022-12-02 19:47:01 -0800

    Day two

--- 2/src/main.cpp
+++ 2/src/main.cpp
@@ -4,10 +4,18 @@
 
 using namespace std;
 
+int part1();
+int part2();
 int getWinner(char player1, char player2);
 
 int main() {
-    ifstream file ("./assets/problem2_input.txt"); 
+    printf("Total score part 1: %d\n", part1());
+    printf("Total score part 2: %d\n", part2());
+}
+
+int part1() {
+
+    ifstream file ("./assets/problem_input.txt"); 
 
     string line;
     int score = 0;
@@ -15,36 +23,80 @@ int main() {
     if(file.is_open()) 
     {
         while (getline(file, line)) {
-            printf("%c %c\n", line[0], line[2]);
+            score += getWinner(line[0], line[2]) + line[2] - 'W';
+        }
+    }
+    else
+        cout<<"Couldn't open the input file\n";
+
+    file.close();
+
+    return score;
+}
+
+
+int part2() {
+    ifstream file ("./assets/problem_input.txt"); 
+
+    string line;
+    int score = 0;
+    unordered_map<char, char> is_beat_by;
+    unordered_map<char, char> wins_against;
+
+    is_beat_by['A'] = 'Z';
+    is_beat_by['B'] = 'X';
+    is_beat_by['C'] = 'Y';
+
+    wins_against['A'] = 'Y';
+    wins_against['B'] = 'Z';
+    wins_against['C'] = 'X';
+
+
+    if(file.is_open()) 
+    {
+        while (getline(file, line)) {
+            // X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win
+
+            // A Y -> A X -> 1 + 3 = 4
+            // B X -> B X -> 1 + 0 = 1
+            // C Z -> C X -> 1 + 6 = 7
+
+            // A X -> A Z -> 3 + 0 = 3
+            // B X -> B X -> 1 + 0 = 1
+            // C X -> C Y -> 2 + 0 = 2
+
+            // A Z -> A Y -> 2 + 6 = 8
+            // B Z -> B Z -> 3 + 6 = 9
+            // C Z -> C X -> 1 + 6 = 7
 
             switch (line[2])
             {
-            case 'X': // Rock
-                score += 1 + getWinner(line[0], 'X');
-                break;
-            case 'Y': // Paper
-                score += 2 + getWinner(line[0], 'Y');
-                break;
-            case 'Z': // Scissors
-                score += 3 + getWinner(line[0], 'Z');
-                break;
-            
-            default:
-                break;
+                case 'X': // Rock
+                    score += is_beat_by[line[0]] - 'W';
+                    break;
+                case 'Y': // Paper
+                    score += int(line[0]) - '@' + 3;
+                    break;
+                case 'Z': // Scissors
+                    score += wins_against[line[0]] - 'W' + 6;
+                    break;
+                
+                default:
+                    break;
             }
         }
     }
     else
-        cout<<"Couldn't open the file\n";
+        cout<<"Couldn't open the input file\n";
 
-    printf("Total score: %d\n", score);
+    file.close();
 
+    return score;
 }
+
 
 int getWinner(char player1, char player2) {
     int score = 0;
-
-    // 
     if(player2 == 'X') {
         // Rock
         if(player1 == 'A') {
@@ -82,3 +134,4 @@ int getWinner(char player1, char player2) {
 
     return score;
 }
+    
