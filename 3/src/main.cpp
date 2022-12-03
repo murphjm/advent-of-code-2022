4f273f00c87f42ae7d91d9c8601ce2f5a658a260
Justin Murphy <jmsconsulting@gmail.com>
2022-12-03 12:49:06 -0800

    Day 3 part two

--- 3/src/main.cpp
+++ 3/src/main.cpp
@@ -1,13 +1,18 @@
-#include <iostream>
+#include <array>
+#include <algorithm>
 #include <fstream>
-#include <unordered_map>
+#include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 int part1();
+int part2();
 
 int main() {
     printf("Sum of the priorities: %d\n", part1());
+    printf("Sum of the badges: %d\n", part2());
 }
 
 int part1() {
@@ -19,7 +24,6 @@ int part1() {
         const auto c = *std::find_first_of(std::begin(line), std::next(std::begin(line), line.size()/2),
                                         std::next(std::begin(line), line.size()/2), std::end(line));
 
-        printf("%c\n", c);
         if (c >= 'a') {
             sum += c - 'a' + 1;
         } else {
@@ -30,3 +34,31 @@ int part1() {
     return sum;
 }
     
+
+int part2() {
+    ifstream file ("./assets/problem_input.txt"); 
+    string line;
+    int sum = 0;
+
+    std::array<int, 58> counter;
+    std::fill(std::begin(counter), std::end(counter), 0);
+    while(std::getline(file, line)) {
+        std::sort(std::begin(line), std::end(line));
+        line.erase(std::unique(std::begin(line), std::end(line)), std::end(line));
+        for (const auto c : line) {
+            counter[c - 'A'] += 1;
+            if (counter[c - 'A'] == 3) {
+                if (c >= 'a') {
+                    sum += c - 'a' + 1;
+                } else {
+                    sum += c - 'A' + 1 + 26;
+                }
+                
+                std::fill(std::begin(counter), std::end(counter), 0);
+                break;
+            }
+        }
+  }
+
+  return sum;
+}
